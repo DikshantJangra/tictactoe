3622be51fcf3534052bacfb65e99ebf441b55c7c
DikshantJangra <dikshantjangra1@gmail.com>
2025-03-21 21:09:28 +0530

    Github deploy configs

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/dikshantjangra.github.io/',
+  base: '/tictactoe',
   plugins: [react(),tailwindcss()],
 })
